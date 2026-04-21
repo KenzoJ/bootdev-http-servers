84208617788a4c189ea21471fef695aad0c70896
KenzoJ <charles.felux@gmail.com>
2026-04-21 14:21:46 -0400

    add healthz

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import {handlerReadiness} from "./api/readiness.js"
 
 const app = express();
 const PORT = 9091;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
+ 
