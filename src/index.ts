b7709330280b6af89c6f56b017eef3c9c3481a87
Kenzo Johnson <charles.felux@gmail.com>
2026-04-23 12:36:37 -0400

    feat: add chirps in api

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { handlerReadiness } from "./api/readiness.js"
 import { middlewareMetricsInc, middlewareLogResponses } from "./api/middleware.js"
 import { handlerHits } from "./app/admin/metrics/index.js";
 import { handlerReset } from "./app/admin/metrics/reset.js"
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -13,10 +14,11 @@ app.use(middlewareLogResponses);
 //API
 app.use("/api", express.static("./src/api"));
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerChirpsValidate)
 
 //ADMIN
 app.get("/admin/metrics", handlerHits);
-app.get("/admin/reset", handlerReset)
+app.post("/admin/reset", handlerReset)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
